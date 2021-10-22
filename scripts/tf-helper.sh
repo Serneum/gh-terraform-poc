66752909a25ec23b52c27629f5bb8890895ada82
Chris Rees <crees@thecitybase.com>
2021-10-22 11:28:40 -0400

    Update tf-helper script to accept multiple `-r` options
    Remove need for token_quote function, thanks to @tallen-citybase

--- scripts/tf-helper.sh
+++ scripts/tf-helper.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+set -eu
+
 function usage() {
   echo "Usage $(basename $0) [-n NAME] [-r REFERENCES] [-d]" 2>&1
   echo "Run a backup or cleanup of a set of data."
   echo "    -d                Dry run. Do not execute the Terraform import commands."
   echo "    -n NAME           The name of the repository you wish to import into Terraform."
-  echo "    -r REFERENCES     A semicolon (;) delimited list of autolink references for the repository."
+  echo "    -r REFERENCES     An autolink reference for the repository. Can be set multiple times."
   echo "                          A reference needs to include its ID and should have the following format: <name>:<id>."
   echo "                          Example:"
-  echo "                              KCORE:99588;KOPS:99581;SE:99586"
+  echo "                              -r KCORE:99588 -r KOPS:99581 -r SE:99586"
   exit 1
 }
 
@@ -19,27 +21,17 @@ function print_error() {
   usage
 }
 
-function token_quote {
-  local quoted=()
-  for token; do
-    quoted+=( "$(printf '%q' "$token")" )
-  done
-  printf '%s\n' "${quoted[*]}"
-}
-
 function execute() {
-  echo -e "$@"
+  echo -e "terraform import $@"
 
   if [ -z $dry_run ];
   then
-    IFS=$OLDIFS
-    eval "$(token_quote $@)"
+    terraform import "$@"
   fi
 }
 
-OLDIFS=$IFS
-IFS=';'
-
+dry_run=
+autolink_references=()
 optstring="dn:r:"
 while getopts ${optstring} opt; do
   case ${opt} in
@@ -48,10 +40,10 @@ while getopts ${optstring} opt; do
       ;;
     n)
       repo_name="${OPTARG}"
-      normalized_repo_name=$(echo $repo_name | tr '_' '-')
+      normalized_repo_name=${repo_name/_/-}
       ;;
     r)
-      autolink_references="${OPTARG}"
+      autolink_references+=("${OPTARG}")
       ;;
     :)
       echo "Error: -${OPTARG} requires an argument."
@@ -71,26 +63,18 @@ if [ -z $repo_name ]; then
     print_error "A repository name must be specified."
 fi
 
-base_import_command="terraform import module.${normalized_repo_name}"
+module_name="module.${normalized_repo_name}"
 
-repo_import_command="${base_import_command}.github_repository.repo ${repo_name}"
-execute $repo_import_command
+execute "${module_name}".github_repository.repo "${repo_name}"
 
-branch_import_command="${base_import_command}.github_branch.master ${repo_name}:master"
-execute $branch_import_command
+execute "${module_name}".github_branch.master "${repo_name}":master
 
-default_branch_import_command="${base_import_command}.github_branch_default.default ${repo_name}"
-execute $default_branch_import_command
+execute "${module_name}".github_branch_default.default "${repo_name}"
 
-protection_import_command="${base_import_command}.github_branch_protection.branch-protection-master ${repo_name}:master"
-execute $protection_import_command
+execute "${module_name}".github_branch_protection.branch-protection-master "${repo_name}":master
 
-for reference in $autolink_references
+for reference in ${autolink_references[@]}
 do
-    # We could probably build up an array of commands, then iterate over that array
-    # an execute those commands to avoid resetting IFS each iteration
-    IFS=';'
-    reference_parts=(`echo $reference | tr ':' ';'`)
-    autolink_import_command="${base_import_command}.github_repository_autolink_reference.autolink_reference[\"${reference_parts[0]}\"] ${repo_name}/${reference_parts[1]}"
-    execute $autolink_import_command
+    reference_parts=($(echo "${reference/:/ }"))
+    execute "${module_name}".github_repository_autolink_reference.autolink_reference"[\"${reference_parts[0]}\"]" "${repo_name}"/"${reference_parts[1]}"
 done
